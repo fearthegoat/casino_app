839345a62e7fb11269e6ab68ac4a48a6a094f95a
fearthegoat <kmanuel09@gmail.com>
2015-08-04 14:53:24 -0400

    rakes routes for players and sets root

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,23 @@
+class PlayersController < ApplicationController
+  def show
+  end
+
+  def index
+    @players = Player.all
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+end
